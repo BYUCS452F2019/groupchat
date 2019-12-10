a71d4e7c0fb28744b02417a0f7ca2a8408e430f3
EC2 Default User <ec2-user@ip-172-31-46-46.us-west-2.compute.internal>
2019-12-10 16:33:42 +0000

    fixing final bugs in sql version

--- server/src/post.ts
+++ server/src/post.ts
@@ -2,6 +2,7 @@ import * as express from "express";
 import { CreatePostRequest } from "./requests/requests";
 import { executeScript, METHODS } from "./db";
 import { CreatePostResponse, DeletePostResponse } from "./responses/responses";
+import { Shortcut } from "./models/models";
 var router = express.Router();
 
 router.post('/create', async function (req, res, next) {
@@ -12,9 +13,25 @@ router.post('/create', async function (req, res, next) {
 
   const createResponse = await executeScript(data, METHODS.createPost);
   const getResponse = await executeScript(createResponse, METHODS.getPostById);
+  
+  const userData = { username: data.username };
+  const shortcuts: Shortcut[] = await executeScript(userData, METHODS.getUserShortcuts);
+  
+  let content = getResponse.content;
+  
+  shortcuts.forEach((s) => {
+    const patternRegex = new RegExp(s.pattern, 'g');
+    content = content.replace(patternRegex, s.command);
+  });
+  
+  const post = {
+    ...getResponse,
+    content,
+  };
+  
   const transformedResponse: CreatePostResponse = (() => {
     return {
-      post: getResponse
+      post
     };
   })(); // TODO format as needed
 
